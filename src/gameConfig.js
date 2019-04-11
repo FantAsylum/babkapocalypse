692b1eb67244fedcdc812b2e0f6cfabd2782e1a1
PatriotCodes <patriot130690@gmail.com>
2019-04-11 18:57:32 +0300

    Updates

--- src/gameConfig.js
+++
@@ -1,8 +0,0 @@
-import Phaser from "phaser";
-
-export const config = {
-  type: Phaser.AUTO,
-  width: 1200,
-  height: 800,
-  parent: 'game'
-};
--- src/scenes/index.js
+++
@@ -1,3 +0,0 @@
-export * from './mainMenu'
-
-export * from './scene01'
